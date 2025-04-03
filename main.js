203dc9d7b9d16e01892fde6b00b9e5957f24b1aa
Kadeem <kadeem.school@gmail.com>
2025-04-03 14:58:02 -0400

    Added dynamic weather feature

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-document.getElementById("temperature-value").textContent = 50;
+async function start() {
+  let weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
+  let weatherData = await weatherPromise.json();
+  let temperature = weatherData.properties.periods[0].temperature;
+  document.querySelector("#temperature-value").textContent = temperature;
+  console.log(temperature);
+}
+
+start()
+
 
