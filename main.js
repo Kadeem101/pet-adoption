a063aa1881db4f847ae30701d216d05e0e7fcb5b
Kadeem <kadeem.school@gmail.com>
2025-04-03 19:46:09 -0400

    Trying to resolve rendering issue

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ async function getPetData() {
     petInfo.appendChild(petDescription);
     petInfo.appendChild(petAge);
 
+
     // Create Pet Image
     const petImage = document.createElement("img");
     petImage.classList.add("pet__image");
