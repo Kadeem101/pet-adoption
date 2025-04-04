dea51fde74733e5e6ebb751d71eab78ca729b728
Kadeem <kadeem.school@gmail.com>
2025-04-04 15:58:27 -0400

    Refactored code by creating an HTML template & introducing the cloneNode() function

--- main.js
+++ main.js
@@ -1,68 +1,30 @@
-async function start() {
+async function get_temperature() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
   const weatherData = await weatherPromise.json();
   const temperature = weatherData.properties.periods[0].temperature;
   document.querySelector("#temperature-value").textContent = temperature;
-
 }
-start()
+get_temperature()
 
 async function getPetData() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
   const petData = await petsPromise.json();
+  const petContainer = document.querySelector(".pets__cards");
+  const template = document.querySelector("#pets__card-template");
+  const fragment = document.createDocumentFragment()
+
   petData.forEach(pet => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector(".pet__name").textContent = pet.name;
+    clone.querySelector(".pet__description").textContent = pet.description || "No description";
+    clone.querySelector(".pet__age").textContent = `${new Date().getFullYear() - pet.birthYear} years old`;
+    clone.querySelector(".pet__image").src = pet.photo || "./images/pets/fallback.jpg";
+    clone.querySelector(".pet__image").alt = `Photo of a ${pet.species} named ${pet.name}`;
 
-    // Create Pet Card Container
-    const petCard = document.createElement("div");
-    petCard.classList.add("pet");
-
-    // Create Pet Info Container
-    const petInfo = document.createElement("div");
-    petInfo.classList.add("pet__info");
-
-    petCard.appendChild(petInfo);
-
-    // Create Pet Name Container
-    const petNameContainer = document.createElement("div");
-    petNameContainer.classList.add("pet__name-container");
-
-    petInfo.appendChild(petNameContainer);
-
-    // Get Pet Name
-    const petName = document.createElement("h3");
-    petName.classList.add("pet__name");
-    petName.textContent = pet.name;
-
-    // Create Pet Contact Envelope
-    const petContactIcon = document.createElement("img");
-    petContactIcon.setAttribute("src", "./images/icons/envelope-plus 1.svg");
-
-    petNameContainer.appendChild(petName);
-    petNameContainer.appendChild(petContactIcon);
-
-    // Get Pet Description
-    const petDescription = document.createElement("p");
-    petDescription.classList.add("pet__description");
-    petDescription.textContent = pet.description;
-
-    // Get Pet Age
-    const petAge = document.createElement("p");
-    petAge.classList.add("pet__age");
-    petAge.textContent = pet.birthYear;
-
-    petInfo.appendChild(petDescription);
-    petInfo.appendChild(petAge);
-
-    // Create Pet Image
-    const petImage = document.createElement("img");
-    petImage.classList.add("pet__image");
-    petImage.setAttribute("src", pet.photo ?? "./images/icons/envelope-plus 1.svg");
-
-
-    petCard.appendChild(petImage);
-
-    document.querySelector(".pets__cards").appendChild(petCard);
+    fragment.appendChild(clone);
   });
+
+  petContainer.appendChild(fragment);
 }
 
 getPetData()
