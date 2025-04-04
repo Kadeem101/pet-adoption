a2aa1741ce4641b19a878be3bf41de4ed22f4afc
Kadeem <kadeem.school@gmail.com>
2025-04-04 16:28:20 -0400

    Changed are calculation structure

--- main.js
+++ main.js
@@ -17,7 +17,13 @@ async function getPetData() {
     const clone = template.content.cloneNode(true);
     clone.querySelector(".pet__name").textContent = pet.name;
     clone.querySelector(".pet__description").textContent = pet.description || "No description";
-    clone.querySelector(".pet__age").textContent = `${new Date().getFullYear() - pet.birthYear} years old`;
+    function calcAge(birthYear) {
+      const age = new Date().getFullYear() - birthYear;
+      if (age == 1) return `${age} year old`;
+      if (age > 1) return `${age} years old`;
+      return "Less than a year";
+    }
+    clone.querySelector(".pet__age").textContent = calcAge(pet.birthYear);
     clone.querySelector(".pet__image").src = pet.photo || "./images/pets/fallback.jpg";
     clone.querySelector(".pet__image").alt = `Photo of a ${pet.species} named ${pet.name}`;
 
