9cd88484bfe20b8886e1e722e8e74de6e27342a8
Kadeem <kadeem.school@gmail.com>
2025-04-04 17:00:34 -0400

    Added dynamic copyright year & changed calcAge function position

--- main.js
+++ main.js
@@ -17,12 +17,6 @@ async function getPetData() {
     const clone = template.content.cloneNode(true);
     clone.querySelector(".pet__name").textContent = pet.name;
     clone.querySelector(".pet__description").textContent = pet.description || "No description";
-    function calcAge(birthYear) {
-      const age = new Date().getFullYear() - birthYear;
-      if (age == 1) return `${age} year old`;
-      if (age > 1) return `${age} years old`;
-      return "Less than a year";
-    }
     clone.querySelector(".pet__age").textContent = calcAge(pet.birthYear);
     clone.querySelector(".pet__image").src = pet.photo || "./images/pets/fallback.jpg";
     clone.querySelector(".pet__image").alt = `Photo of a ${pet.species} named ${pet.name}`;
@@ -34,3 +28,13 @@ async function getPetData() {
 }
 
 getPetData()
+
+function calcAge(birthYear) {
+  const age = new Date().getFullYear() - birthYear;
+  if (age == 1) return `${age} year old`;
+  if (age > 1) return `${age} years old`;
+  return "Less than a year";
+}
+
+// Copyright footer year
+document.querySelector("#year").textContent = new Date().getFullYear();
